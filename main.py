a400496b1b27c8c2d5a988bfa3fb747c820a31eb
ethan <132926082+EthanNCai@users.noreply.github.com>
2023-09-20 20:21:14 +0800

    comments added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 from collections import defaultdict, OrderedDict
 
-
+#这个函数是用来读取文件的
 def get_file_contents(path):
     try:
         with open(path, 'r', encoding='UTF-8') as f:
@@ -13,14 +13,14 @@ def get_file_contents(path):
         print(f"Error:文件读取失败，位置： {path}")
         return ""
 
-
+#使用字典来初步构建分词的词频表
 def build_frequency_dict(tokens):
     frequency_dict = defaultdict(int)
     for token in tokens:
         frequency_dict[token] += 1
     return frequency_dict
 
-
+#使用有序字典，将两个字典处理为维度相同的向量
 def align_dictionaries(dict1, dict2):
     sorted_dict1 = OrderedDict(sorted(dict1.items()))
     sorted_dict2 = OrderedDict(sorted(dict2.items()))
@@ -28,18 +28,19 @@ def align_dictionaries(dict1, dict2):
     aligned_dict2 = OrderedDict()
     all_keys = set(sorted_dict1.keys()) | set(sorted_dict2.keys())
     for key in all_keys:
+        //如果在文件b的某一个词在文件a里未出现，那么a的向量的该位置为0
         aligned_dict1[key] = sorted_dict1.get(key, 0)
         aligned_dict2[key] = sorted_dict2.get(key, 0)
     return aligned_dict1, aligned_dict2
 
-
+# 用正则表达式初步规律以及使用jieba库进行自动分词
 def filter_text(string):
     pattern = re.compile(u"[^a-zA-Z0-9\u4e00-\u9fa5]")
     string = pattern.sub("", string)
     result = jieba.lcut(string)
     return result
 
-
+# 基于余弦函数的空间相似度计算
 def calculate_similarity(text1, text2):
     dict1 = build_frequency_dict(text1)
     dict2 = build_frequency_dict(text2)
@@ -52,7 +53,7 @@ def calculate_similarity(text1, text2):
     cosine_similarity = dot_product / (norm1 * norm2)
     return cosine_similarity
 
-
+## 保存文件
 def save_float_to_file(float_number, file_path):
     try:
         with open(file_path, 'w') as file:
